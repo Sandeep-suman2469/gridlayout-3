98e8a9b82dfa0a3c588a3ec10450841f8749992c
Sandeep-suman2469 <sandeepkumarsuman@tothebyte.com>
2025-08-13 16:45:16 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,93 +3,86 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <div className="nav">
+        <div className="layout navbar">Layout1</div>
+        <div className="about navbar">about</div>
+        <div className="services navbar">services</div>
+        <div className="contact navbar">contact</div>
+      </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className="heading">
+        <h1>Sample Heading</h1>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit quisque
+          faucibus ex sapien vitae pellentesque sem placerat in id cursus mi
+          pretium tellus duis convallis tempus leo eu aenean sed diam.
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <button className="btn">Click to action!</button>
+      </div>
+      <div className="line"></div>
+      <div className="p">Latest Features</div>
+      <div className="container">
+        <div className="box">
+          <div className="box1">800 X 500</div>
+          <div className="content">
+            <div className="box-heading">Feature Label</div>
+            <div className="para">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit.
+            </div>
+            <div>
+            <button className="btns">Buy Now!</button>
+            <button className="bt">More Info</button>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="box1">800 X 500</div>
+          <div className="content">
+            <div className="box-heading">Feature Label</div>
+            <div className="para">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit.
+            </div>
+            <div>
+            <button className="btns">Buy Now!</button>
+            <button className="bt">More Info</button>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="box1">800 X 500</div>
+          <div className="content">
+            <div className="box-heading">Feature Label</div>
+            <div className="para">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit.
+            </div>
+            <div>
+            <button className="btns">Buy Now!</button>
+            <button className="bt">More Info</button>
+            </div>
+          </div>
+        </div>
+         <div className="box">
+          <div className="box1">800 X 500</div>
+          <div className="content">
+            <div className="box-heading">Feature Label</div>
+            <div className="para">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit.
+            </div>
+            <div>
+            <button className="btns">Buy Now!</button>
+            <button className="bt">More Info</button>
+            </div>
+          </div>
+        </div>
+      </div>
+            <br></br>
+      <br></br>
+      <br></br>
+      <div className="line"></div>
+      <div className="last-line">Copyright © Your Website 2014</div>
+        
+    </>
   );
 }
+
